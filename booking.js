8c21f7da27e2c395a2b06c77fd8744caa4dc5b59
Manar <mael0009@stud.kea.dk>
2025-05-23 12:38:43 +0200

    header og footer mtl og css

--- booking.js
+++ booking.js
@@ -1,29 +1,5 @@
 
-// BRUGER MENU//
-const burger = document.getElementById("burger");
-const nav = document.getElementById("nav");
 
-burger.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
-
-
-// ANMELDELSER//
-  
-//   const track = document.querySelector(".carousel-track");
-//   const next = document.querySelector(".next");
-//   const prev = document.querySelector(".prev");
-
-//   let scrollAmount = 0;
-//   const cardWidth = 320; 
-
-//   next.addEventListener("click", () => {
-//     scrollAmount += cardWidth;
-//     track.scrollTo({
-//       left: scrollAmount,
-//       behavior: "smooth"
-//     });
-//   });
 
 
 
