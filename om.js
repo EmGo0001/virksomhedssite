5cf90efd99590686ab607f2263f11d7eb4d9f8cd
Emily <emgo0001@stud.kea.dk>
2025-05-23 13:34:43 +0200

    jj

---
+++ om.js
@@ -0,0 +1,7 @@
+// BRUGER MENU//
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
